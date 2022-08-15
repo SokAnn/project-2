27b14b1550be63618cbe023b41bac6bf81e84d19
Ann <fullsokan@gmail.com>
2022-08-15 13:29:31 +0300

    modified rtl logic

--- sort_avalon/rtl/sort_avalon.sv
+++ sort_avalon/rtl/sort_avalon.sv
@@ -21,14 +21,15 @@ module sort_avalon #(
 );
 
 logic [DWIDTH-1:0] memory [MAX_PKT_LEN-1:0];
-logic [DWIDTH-1:0] sorted_array [MAX_PKT_LEN-1:0];
+logic [MAX_PKT_LEN-1:0][DWIDTH-1:0] sorted_array;
 
-logic [$clog2(MAX_PKT_LEN):0] written_words, read_words;
+logic [$clog2(MAX_PKT_LEN):0] written_words, read_word;
 logic                         out_trans;
 logic [$clog2(MAX_PKT_LEN):0] sorted_word;
 logic [DWIDTH-1:0]            word;
+logic                         sorted_flag;
 
-typedef enum logic [1:0] {INPUT_TRANS_S, SORT_S, OUTPUT_TRANS_S} state_type;
+typedef enum logic [1:0] {INPUT_TRANS_S, READ_FROM_MEM_S, SORTING_DATA_S, OUTPUT_TRANS_S} state_type;
 state_type state, next_state;
 
 
@@ -47,13 +48,21 @@ always_comb
       INPUT_TRANS_S:
         begin
           if( snk_valid_i && snk_endofpacket_i )
-            next_state = SORT_S;
+            next_state = READ_FROM_MEM_S;;
         end
         
-      SORT_S:
+      READ_FROM_MEM_S:
         begin
-          if( out_trans )
+          next_state = SORTING_DATA_S;
+        end
+      
+      SORTING_DATA_S:
+        begin
+          if( read_word == written_words - 1 && sorted_flag )
             next_state = OUTPUT_TRANS_S;
+          else
+           if( sorted_flag || read_word == 0 )
+             next_state = READ_FROM_MEM_S;
         end
         
       OUTPUT_TRANS_S:
@@ -91,31 +100,90 @@ always_ff @( posedge clk_i )
 always_ff @( posedge clk_i )
   begin
     if( srst_i )
-      read_words <= '0;
+      read_word <= '0;
     else
       begin
-        if( next_state == OUTPUT_TRANS_S || state == OUTPUT_TRANS_S )
-          begin
-            if( read_words <= written_words && !src_endofpacket_o )
-              read_words <= read_words + 1'(1);
-            else
-              read_words <= '0;
-          end
+        if( state == INPUT_TRANS_S && next_state == READ_FROM_MEM_S )
+          read_word <= '0;
+        else
+          if( next_state == READ_FROM_MEM_S )
+            read_word <= read_word + 1'(1);
+
+        if( state == SORTING_DATA_S && next_state == OUTPUT_TRANS_S )
+          read_word <= 1'd1;
+        else
+          if( state == OUTPUT_TRANS_S )
+           read_word <= read_word + 1'(1);
+      end
+  end
+
+always_ff @( posedge clk_i )
+  begin
+    if( state == READ_FROM_MEM_S )
+      word <= memory[read_word];
+    else
+      begin
+        if( state == SORTING_DATA_S && written_words > 1 )
+          if( word < sorted_array[sorted_word] )
+            word <= sorted_array[sorted_word];
       end
   end
 
 always_ff @( posedge clk_i )
   begin
     if( srst_i )
-      src_data_o <= 'x;
+      sorted_word <= '0;
     else
       begin
-        if( next_state == OUTPUT_TRANS_S || state == OUTPUT_TRANS_S )
+        if( state == READ_FROM_MEM_S && next_state == SORTING_DATA_S )
+          sorted_word <= '0;
+        else
+          if( state == SORTING_DATA_S )
+            sorted_word <= sorted_word + 1'(1);
+      end
+  end
+
+assign sorted_flag = ( state == SORTING_DATA_S ) ? ( sorted_word == read_word ) : ( 1'b0 ) ;
+
+always_ff @( posedge clk_i )
+  begin
+    if( srst_i )
+      sorted_array <= 'x;
+    else
+      begin
+        if( state == INPUT_TRANS_S )
+          sorted_array <= 'x;
+        else
           begin
-            if( read_words < written_words )
-              src_data_o <= sorted_array[read_words];
+            if( state == SORTING_DATA_S && written_words > 1 )
+              begin
+                if( read_word == 0 )
+                  sorted_array[sorted_word] <= word;
+                else
+                  begin
+                    if( word < sorted_array[sorted_word] )
+                      sorted_array[sorted_word] <= word;
+                    else
+                      if( read_word == sorted_word )
+                        sorted_array[sorted_word] <= word;
+                  end
+              end
           end
       end
+  end
+
+always_ff @( posedge clk_i )
+  begin
+    if( state == SORTING_DATA_S && next_state == OUTPUT_TRANS_S )
+      begin
+        if( written_words > 1)
+          src_data_o <= sorted_array[written_words-read_word-1];
+        else
+          src_data_o <= word;
+      end
+    else
+      if( state == OUTPUT_TRANS_S )
+        src_data_o <= sorted_array[read_word];
   end
 
 always_ff @( posedge clk_i )
@@ -126,7 +194,7 @@ always_ff @( posedge clk_i )
       begin
         if( next_state == OUTPUT_TRANS_S || state == OUTPUT_TRANS_S )
           begin
-            if( read_words <= written_words && !src_endofpacket_o )
+            if( read_word <= written_words && !src_endofpacket_o )
               src_valid_o <= 1'b1;
             else
               src_valid_o <= 1'b0;
@@ -134,8 +202,8 @@ always_ff @( posedge clk_i )
       end
   end
 
-assign src_startofpacket_o = ( src_valid_o && read_words == 1 );
-assign src_endofpacket_o   = ( src_valid_o && read_words == written_words && written_words != 0 );
+assign src_startofpacket_o = ( src_valid_o && read_word == 1 );
+assign src_endofpacket_o   = ( src_valid_o && read_word == written_words && written_words != 0 );
 
 always_ff @( posedge clk_i )
   begin
@@ -150,66 +218,5 @@ always_ff @( posedge clk_i )
       end
   end
 
-always_ff @( posedge clk_i )
-  begin
-    if( snk_endofpacket_i )
-      sorted_word <= '0;
-    else
-      if( state == SORT_S )
-        sorted_word <= sorted_word + 1'(1);
-  end
-
-always_ff @( posedge clk_i )
-  begin
-    if( next_state == SORT_S || state == SORT_S )
-      word <= memory[sorted_word];
-  end
-
-always_ff @( posedge clk_i )
-  begin
-    if( srst_i )
-      out_trans <= 1'b0;
-    else
-      begin
-        if( state == SORT_S )
-          out_trans <= my_insert_sort( sorted_word, written_words, word, sorted_array );
-        else 
-          out_trans <= 1'b0;  
-      end
-  end
-
-function automatic logic my_insert_sort( logic [$clog2(MAX_PKT_LEN):0] sorted_word,
-                                         logic [$clog2(MAX_PKT_LEN):0] written_words,
-                                         logic [DWIDTH-1:0] word,
-                                         ref logic [DWIDTH-1:0] array [MAX_PKT_LEN-1:0] );
-  int j;
-  logic [DWIDTH-1:0] temp;
-  
-  if( sorted_word == 0 )
-    for(int i = 0; i < MAX_PKT_LEN; i++)
-      array[i] = 'x;
-  
-  if( sorted_word <= written_words )
-    array[sorted_word-1] = word;
-
-  if( sorted_word > written_words )
-    return 1'b1;
-  else
-    begin
-      if( sorted_word-1 > 0 && sorted_word <= written_words )
-        begin
-          for(int j = MAX_PKT_LEN-2; j >= 0; j--)
-            begin
-              if( array[j] > array[j+1] )
-                begin
-                  temp = array[j];
-                  array[j] = array[j+1];
-                  array[j+1] = temp;
-                end
-            end
-        end
-      return 1'b0;
-    end
-endfunction
-
 endmodule
+
